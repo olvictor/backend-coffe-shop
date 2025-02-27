643305961d0b76ccfcbe62ed694587fc4e1ebfca
victorolv <victorolvdev@gmail.com>
2025-02-27 12:34:02 -0300

    build: criacao do metodo getSubject

--- src/main/java/com/example/coffe_shop/security/JwtUtil.java
+++ src/main/java/com/example/coffe_shop/security/JwtUtil.java
@@ -5,10 +5,9 @@ import com.example.coffe_shop.model.Usuario;
 import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Value;
 
+import javax.crypto.SecretKey;
 import java.security.Key;
 import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
@@ -20,7 +19,7 @@ public class JwtUtil {
 
     public String gerarToken(Usuario usuario){
         String jwt = Jwts.builder()
-                .subject(String.valueOf(usuario))
+                .subject(String.valueOf(usuario.getEmail()))
                 .signWith(secretKey)
                 .expiration(dataExpiracao())
                 .compact();
@@ -30,5 +29,11 @@ public class JwtUtil {
 
     private Date dataExpiracao() {
         return Date.from(Instant.now().plus(24, ChronoUnit.HOURS));
+    }
+
+
+    public String getSubject(String token){
+          return Jwts.parser().verifyWith((SecretKey) secretKey).build()
+                    .parseSignedClaims(token).getBody().getSubject();
     }
 }
