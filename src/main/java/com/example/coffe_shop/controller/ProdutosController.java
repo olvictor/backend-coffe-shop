8beb4c9e63f14524f14075b2a20d19d1c390356d
victorolv <victorolvdev@gmail.com>
2025-03-06 15:16:08 -0300

    build: criacao do metodo cadastrarProduto no ProdutosController

--- src/main/java/com/example/coffe_shop/controller/ProdutosController.java
+++ src/main/java/com/example/coffe_shop/controller/ProdutosController.java
@@ -1,21 +1,30 @@
 package com.example.coffe_shop.controller;
 
+import com.example.coffe_shop.model.Produto;
+import com.example.coffe_shop.service.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/produtos")
 public class ProdutosController {
+
+    @Autowired
+    private ProdutoService produtoService;
 
     @GetMapping
     public ResponseEntity<?> buscarProdutos(){
         return ResponseEntity.ok().body("aaaaaaaaaaaaaaa");
     }
 
+    @PostMapping()
+    public  ResponseEntity<?>cadastrarProduto(@RequestBody Produto produto){
 
-    @GetMapping("/teste")
-    public  ResponseEntity<?> teste(){
-        return ResponseEntity.ok().body("Testando");   }
+      Produto novoProduto = produtoService.cadastrarProduto(produto.getNome(),produto.getPreco(),produto.getQuantidade(), produto.getImage_url());
+
+      return ResponseEntity.ok().body(novoProduto);
+    }
 }
