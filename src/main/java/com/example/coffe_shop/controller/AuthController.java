d4649bcf7c9f521114befa205e98300c7c85246d
victorolv <victorolvdev@gmail.com>
2025-03-06 12:07:34 -0300

    refactor: mudança do conteudo UsuariosController para AuthController

---
+++ src/main/java/com/example/coffe_shop/controller/AuthController.java
@@ -0,0 +1,38 @@
+package com.example.coffe_shop.controller;
+
+import com.example.coffe_shop.model.Usuario;
+import com.example.coffe_shop.security.JwtUtil;
+import com.example.coffe_shop.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> cadastrarUsuario(@RequestBody Usuario usuario){
+        Usuario user = usuarioService.RegistrarUsuario(usuario.getEmail(), usuario.getPassword(), usuario.getNome());
+
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> logarUsuario(@RequestBody Usuario usuario){
+        Optional<Usuario> usuarioValidado = usuarioService.ValidarUsuario(usuario);
+
+        if(usuarioValidado != null){
+            String token = JwtUtil.gerarToken(usuarioValidado.get().getEmail());
+
+            return ResponseEntity.ok().body(token);
+        }
+
+        return ResponseEntity.status(401).body("Credenciais inválidasss.");
+
+    }
+}
