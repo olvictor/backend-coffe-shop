275e78699e59f12382654b31649f7c5232a01dfa
victorolv <victorolvdev@gmail.com>
2025-03-14 11:11:57 -0300

    build: criacao do metodo produtoEmEstoque no service de produtos

--- src/main/java/com/example/coffe_shop/service/ProdutoService.java
+++ src/main/java/com/example/coffe_shop/service/ProdutoService.java
@@ -5,7 +5,7 @@ import com.example.coffe_shop.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.*;
 
 @Service
 public class ProdutoService {
@@ -23,5 +23,30 @@ public class ProdutoService {
 
     public List<Produto> listarProduto (){
        return produtoRepository.findAll();
+    }
+
+    public Boolean produtoEmEstoque(List<Produto> data){
+        Map<String, Integer> produtos = new HashMap<>();
+
+        for (Produto produto : data) {
+            String chave = String.valueOf(produto.getId());
+            if(!produtos.containsKey(chave)){
+                produtos.put(chave,1);
+            }else{
+                produtos.put(chave, produtos.get(chave) + 1);
+            }
+        }
+
+        for (Map.Entry<String, Integer> entry : produtos.entrySet()) {
+            String chave = entry.getKey();
+            Integer valor = entry.getValue();
+
+            System.out.println("Produto: " + chave + ", Quantidade: " + valor);
+            Optional<Produto> produto = produtoRepository.findById(UUID.fromString(chave));
+            if(valor > produto.get().getQuantidade() ){
+                return false;
+            }
+        }
+        return true;
     }
 }
