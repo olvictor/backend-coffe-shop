684fe986c1ef9bfbb9511e59fba0bef8a78deaf3
victorolv <victorolvdev@gmail.com>
2025-03-14 11:25:03 -0300

    build: criacao do cadastrarPedido em PedidoService

--- src/main/java/com/example/coffe_shop/service/PedidoService.java
+++ src/main/java/com/example/coffe_shop/service/PedidoService.java
@@ -1,12 +1,35 @@
 package com.example.coffe_shop.service;
 
+import com.example.coffe_shop.DTO.PedidosRequestDTO;
+import com.example.coffe_shop.model.Pedido;
+import com.example.coffe_shop.model.Produto;
 import com.example.coffe_shop.repository.PedidoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class PedidoService {
     @Autowired
     private PedidoRepository pedidoRepository;
 
+    @Autowired
+    ProdutoService produtoService;
+
+    public Optional<?> cadastrarPedido(PedidosRequestDTO data){
+
+        Double valorTotal = 0.0;
+
+        for (Produto produto : data.produtos()) {
+            valorTotal += produto.getPreco();
+        }
+        Boolean validado = produtoService.produtoEmEstoque(data.produtos());
+        if(validado){
+            Pedido novoPedido = new Pedido(data.produtos(), valorTotal);
+            return Optional.of(pedidoRepository.save(novoPedido));
+        }
+
+        return null;
+    }
 }
