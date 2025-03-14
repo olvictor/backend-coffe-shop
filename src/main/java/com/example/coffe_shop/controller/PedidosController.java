1313649c4f5e5e9769a6acbb2311e8eb6bbe30b8
victorolv <victorolvdev@gmail.com>
2025-03-14 11:27:20 -0300

    fix: correcao na rota de criação de pedidos

--- src/main/java/com/example/coffe_shop/controller/PedidosController.java
+++ src/main/java/com/example/coffe_shop/controller/PedidosController.java
@@ -1,16 +1,28 @@
 package com.example.coffe_shop.controller;
 
+import com.example.coffe_shop.DTO.PedidosRequestDTO;
+import com.example.coffe_shop.model.Pedido;
+import com.example.coffe_shop.model.Produto;
+import com.example.coffe_shop.service.PedidoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/pedidos")
 public class PedidosController {
 
+    @Autowired
+    private PedidoService pedidoService;
+
     @PostMapping()
-    public ResponseEntity<?> cadastrarPedido(){
-        return ResponseEntity.ok().body("testando rota.");
+    public ResponseEntity<?> cadastrarPedido(@RequestBody PedidosRequestDTO data){
+       Optional<Pedido> cadastrarPedido = (Optional<Pedido>) pedidoService.cadastrarPedido(data);
+       return ResponseEntity.ok().body(cadastrarPedido);
     }
 }
